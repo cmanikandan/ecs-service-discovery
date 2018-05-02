26c4a6c80d6af1230d2fc53c41bd10e91edbf8ab
matthias <matthias@calvin>
2018-05-02 22:11:23 +0200

    iterate over clusters, services, tasks and ips

--- ecs-service-discovery.sh
+++ ecs-service-discovery.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+declare CLUSTERS
 declare CLUSTER
+declare SERVICES
+declare SERVICE
+declare TASKS
+declare TASK
+declare IPS
+declare IP
 
 __help() {
     cat << __END
@@ -17,9 +24,58 @@ Options:
 
 If no Route 53 zone is specified using -z, the cluster name is being used.
 
-For bug reports and improvements, please visit:
+Requires the AWS CLI tool, being logged in and having the following policies:
+  ecs:ListServices
+  ecs:ListTasks
+  ecs:DescribeTasks
+  ecs:DescribeServices
+  route53:ListHostedZones
+  route53:ChangeResourceRecordSets
+  route53:GetChange
+
+For more information, improvements and bug reports, please visit:
  <https://github.com/wrzlbrmft/ecs-service-discovery>
 __END
+}
+
+__list_services() {
+    local CLUSTER="$1"
+    local FILTER="$2"
+
+    # TODO: apply filter
+    local SERVICES="$(aws ecs list-services --cluster "${CLUSTER}" --query 'serviceArns[*]' --output text)"
+    if [ "$?" -ne 0 ]; then
+        printf "fatal error: unable to list services of cluster '%s'\n" "${CLUSTER}" >&2
+        exit 1
+    fi
+
+    echo "${SERVICES}"
+}
+
+__list_tasks() {
+    local CLUSTER="$1"
+    local SERVICE="$2"
+
+    local TASKS="$(aws ecs list-tasks --cluster "${CLUSTER}" --service-name "${SERVICE}" --query 'taskArns[*]' --output text)"
+    if [ "$?" -ne 0 ]; then
+        printf "fatal error: unable to list tasks of service '%s' of cluster '%s'\n" "${SERVICE}" "${CLUSTER}" >&2
+        exit 1
+    fi
+
+    echo "${TASKS}"
+}
+
+__describe_task() {
+    local CLUSTER="$1"
+    local TASK="$2"
+
+    local IPS="$(aws ecs describe-tasks --cluster "${CLUSTER}" --task "${TASK}" --query 'tasks[*].containers[?lastStatus==`RUNNING`].networkInterfaces[*].privateIpv4Address' --output text)"
+    if [ "$?" -ne 0 ]; then
+        printf "fatal error: unable to describe task '%s' of cluster '%s'\n" "${TASK}" "${CLUSTER}" >&2
+        exit 1
+    fi
+
+    echo "${IPS}"
 }
 
 # main
@@ -94,8 +150,26 @@ if [ -z "$*" ]; then
     exit 1
 fi
 
-for CLUSTER in "$@"; do
-    printf "%s\n" "${CLUSTER}"
+CLUSTERS="$*"
+for CLUSTER in ${CLUSTERS}; do
+    printf "cluster: %s\n" "${CLUSTER}"
+
+    SERVICES="$(__list_services "${CLUSTER}" "${FILTER}")"
+    for SERVICE in ${SERVICES}; do
+        SERVICE="${SERVICE##*/}"
+        printf "    service: %s\n" "${SERVICE}"
+
+        TASKS="$(__list_tasks "${CLUSTER}" "${SERVICE}")"
+        for TASK in ${TASKS}; do
+            TASK="${TASK##*/}"
+            printf "        task: %s\n" "${TASK}"
+
+            IPS="$(__describe_task "${CLUSTER}" "${TASK}")"
+            for IP in ${IPS}; do
+                printf "            ip: %s\n" "${IP}"
+            done
+        done
+    done
 done
 
 exit 0
